f1461b09c786062b66a92cbe5f349adccfb1ac20
Rodolfo Berrios <inbox@rodolfoberrios.com>
2020-06-23 15:34:16 -0400

    vuepress

---
+++ .vuepress/config-project.js
@@ -0,0 +1,51 @@
+module.exports = {
+    title: 'Chevereto Docs',
+    description: 'Documentation for Chevereto',
+    head: [
+        ['link', {
+            rel: 'icon',
+            href: `/logo.svg`
+        }],
+        ['link', {
+            rel: 'manifest',
+            href: '/manifest.json'
+        }],
+        ['meta', {
+            name: 'theme-color',
+            content: '#23a8e0'
+        }],
+        ['meta', {
+            name: 'apple-mobile-web-app-capable',
+            content: 'yes'
+        }],
+        ['meta', {
+            name: 'apple-mobile-web-app-status-bar-style',
+            content: 'black'
+        }],
+        ['link', {
+            rel: 'apple-touch-icon',
+            href: '/logo.svg'
+        }],
+        ['link', {
+            rel: 'mask-icon',
+            href: '/logo.svg',
+            color: '#3eaf7c'
+        }],
+        ['meta', {
+            name: 'msapplication-TileImage',
+            content: '/logo.svg'
+        }],
+        ['meta', {
+            name: 'msapplication-TileColor',
+            content: '#000000'
+        }]
+    ],
+    themeConfig: {
+        logo: '/logo.svg',
+        repo: 'chevereto/chevereto',
+        docsRepo: 'chevereto/docs',
+        smoothScroll: true,
+        editLinks: true,
+        lastUpdated: true,
+    },
+};

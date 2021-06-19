e48d98f62f8c87075d35b0d43492175c32283c5d
Rodolfo Berrios <inbox@rodolfoberrios.com>
2021-06-19 17:57:17 -0400

    removes root guides

--- setup/stacks/naming.php
+++ setup/stacks/naming.php
@@ -1,6 +1,2 @@
 <?php
-
-return [
-    'ubuntu-18.04-lemp.md' => 'Ubuntu 18.04 LEMP',
-    'ubuntu-20.04-openlitespeed.md' => 'Ubuntu 20.04 OpenliteSpeed'
-];
+return [];
--- setup/stacks/sorting.php
+++ setup/stacks/sorting.php
@@ -3,6 +3,4 @@
 return [
     'container.md',
     'container-registry.md',
-    'ubuntu-20.04-openlitespeed.md',
-    'ubuntu-18.04-lemp.md',
 ];
